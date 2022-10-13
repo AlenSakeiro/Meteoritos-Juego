e21285e815aba7a3e3dc15df89eaefd141ed59e1
AlenSakeiro <alenelio777@gmail.com>
2022-10-12 23:36:15 -0300

    commit_alen

--- Juego/Proyectiles/Canion.gd
+++ Juego/Proyectiles/Canion.gd
@@ -5,6 +5,7 @@ extends Node2D
 ## Atributos export
 export var proyectil:PackedScene = null
 export var cadencia_disparo:float = 0.8
+export var velocidad_proyectil:int = 100
 export var danio_proyectil:int = 1
 
 
@@ -20,9 +21,10 @@ var puntos_disparo:Array = []
 
 
 ## Setters y Getters
-func set_esta_disparando(disparando:bool) -> void:
+func set_esta_disparando(disparando: bool) -> void:
 	esta_disparando = disparando
 
+## Metodos
 func _ready() -> void:
 	almacenar_puntos_disparo()
 	timer_enfriamiento.wait_time = cadencia_disparo
@@ -43,16 +45,20 @@ func disparar() -> void:
 	timer_enfriamiento.start()
 	for punto_disparo in puntos_disparo:
 		var new_proyectil:Proyectil = proyectil.instance()
-		#new_proyectil.crear(
-			#punto_disparo.global_position,
-			#get_owner().rotation,
-			#velocidad_proyectil,
-			#danio_proyectil
-			#)
+		new_proyectil.crear(
+			punto_disparo.global_position,
+			get_owner().rotation,
+			velocidad_proyectil,
+			danio_proyectil
+			)
+		Eventos.emit_signal("disparo", new_proyectil)
+
+func _on_TimerEnfriamiento_timeout() -> void:
+	esta_enfriado = true
 
 ## Construir
 func crear(pos: Vector2, dir: float, vel: float, danio_p: int) -> void:
 	position = pos
 	rotation = dir
-	#velocidad = Vector2(vel, 0).rotated(dir)
-	#danio = danio_p
+	velocidad = Vector2(vel, 0).rotated(dir)
+	danio = danio_p
