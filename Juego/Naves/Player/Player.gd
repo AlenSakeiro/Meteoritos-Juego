72440f4b8e98380aea04b3fa8f3692c9e68560ae
AlenSakeiro <alenelio777@gmail.com>
2022-10-18 00:32:57 -0300

    Disparos
     - Disparo Enemigo
     - Daño

--- Juego/Naves/Player/Player.gd
+++ Juego/Naves/Player/Player.gd
@@ -5,6 +5,7 @@ extends RigidBody2D
 export var potencia_motor:int = 20
 export var potencia_rotacion:int = 280
 export var estela_maxima:int = 150
+export var hitpoints:float = 15.0
 
 
 ## Atributos Onready
@@ -13,6 +14,7 @@ onready var laser:RayoLaser = $LaserBeam2D
 onready var estela:Estela = $EstelaPuntoInicio/Trail2D
 onready var motor_sfx:Motor = $MotorSFX
 onready var colisionador:CollisionShape2D = $CollisionShape2D
+onready var impacto_sfx:AudioStreamPlayer = $ImpactosSFX
 
 
 ## Atributos
@@ -100,6 +102,13 @@ func player_input() -> void:
 	
 	if Input.is_action_just_released("disparo_principal"):
 		canion.set_esta_disparando(false)
+
+func recibir_danio(danio: float) -> void:
+	hitpoints -= danio
+	if hitpoints <= 0.0:
+		destruir()
+		
+	impacto_sfx.play()
 
 ## Señales Internas
 func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
